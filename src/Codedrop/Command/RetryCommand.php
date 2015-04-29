be16f46abcf071460c17fb89abe7868ce34285b1
Ben Dougherty <ben@dougherty.net.au>
2015-04-29 21:12:01 +0800

    Rename param.

--- src/Codedrop/Command/RetryCommand.php
+++ src/Codedrop/Command/RetryCommand.php
@@ -23,7 +23,7 @@ class RetryCommand extends CommandBase {
     $this
       ->setName('retry')
       ->setDescription('Retry a build.')
-      ->addOption('build-num', 'n', InputOption::VALUE_OPTIONAL, 'The build number or "latest" to retry the last build.', 'latest')
+      ->addOption('build-num', 'o', InputOption::VALUE_OPTIONAL, 'The build number or "latest" to retry the last build.', 'latest')
       ->addOption('retry-method', 'm', InputOption::VALUE_OPTIONAL, 'Simply retry or retry with ssh by using "retry" or "ssh"', 'retry')
       ->addOption('project-name', 'p', InputOption::VALUE_OPTIONAL, 'Project name?')
       ->addOption('username', 'u', InputOption::VALUE_OPTIONAL, 'The project username');
